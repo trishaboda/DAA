9bc168136366cc9ef951585787e76117ff59b315
Trisha Boda <141979558+trishaboda@users.noreply.github.com>
2024-08-30 18:58:05 +0530

    updated

--- 3Prims.cpp
+++ 3Prims.cpp
@@ -4,84 +4,65 @@ using namespace std;
 
 const int MAX = 100; 
 
-class Prims
-{
+class Prims{
 public:
-    void createGraph(int cost[MAX][MAX], int &n)
-    {
+    void createGraph(int cost[MAX][MAX], int &n){
         cout << "Enter the number of vertices: ";
         cin >> n;
 
         cout << "Enter the adjacency cost matrix:" << endl;
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
                 cin >> cost[i][j];
-                if (cost[i][j] == 0 && i != j) 
-                {
+                if (cost[i][j] == 0 && i != j) {
                     cost[i][j] = INFINITY;
                 }
             }
         }
     }
 
-    int findMinCostVertex(int n, int minCost[MAX], bool inTree[MAX])
-    {
+    int findMinCostVertex(int n, int minCost[MAX], bool inTree[MAX]){
         int minCostVertex = -1;
-        for (int v = 0; v < n; v++)
-        {
-            if (!inTree[v] && (minCostVertex == -1 || minCost[v] < minCost[minCostVertex]))
-            {
+        for (int v = 0; v < n; v++){
+            if (!inTree[v] && (minCostVertex == -1 || minCost[v] < minCost[minCostVertex])){
                 minCostVertex = v;
             }
         }
         return minCostVertex;
     }
 
-    void findMinimumSpanningTree(int n, int cost[MAX][MAX])
-    {
+    void findMinimumSpanningTree(int n, int cost[MAX][MAX]){
         int minCost[MAX];
         bool inTree[MAX];
 
-        for (int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < n; i++){
             minCost[i] = 1000; 
             inTree[i] = false;
         }
 
         int startVertex = 0; 
         minCost[startVertex] = 0;
-
         int edgesAdded = 0;
         int sum = 0;
 
-        while (edgesAdded < n - 1)
-        {
+        while (edgesAdded < n - 1){
             int u = findMinCostVertex(n, minCost, inTree);
-
-            if (u == -1)
-            {
+            if (u == -1){
                 cout << "No spanning tree found!" << endl;
                 return;
             }
 
             inTree[u] = true;
 
-            for (int v = 0; v < n; v++)
-            {
-                if (!inTree[v] && cost[u][v] < minCost[v])
-                {
+            for (int v = 0; v < n; v++){
+                if (!inTree[v] && cost[u][v] < minCost[v]){
                     minCost[v] = cost[u][v];
                 }
             }
-
             edgesAdded++;
         }
-
         cout << "Minimum Spanning Tree Edges:" << endl;
-        for (int v = 1; v < n; v++)
-        {
+        for (int v = 1; v < n; v++){
             cout << "Edge " << v - 1 << " - " << v << " with cost " << minCost[v] << endl;
             sum += minCost[v];
         }
@@ -89,11 +70,9 @@ public:
     }
 };
 
-int main()
-{
+int main(){
     int n; 
     int cost[MAX][MAX];
-
     Prims p;
     p.createGraph(cost, n);
     p.findMinimumSpanningTree(n, cost);
